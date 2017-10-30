5a4959ca41119cba61d47072f2e81e793f03ed47
Mohsen Hasani <mohsen2hasani@gmail.com>
2017-10-30 17:06:57 +0330

    some improvements ...

---
+++ TSQL_Inliner/Model/CommentModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSQL_Inliner.Model
+{
+    public class CommentModel
+    {
+        public CommentModel()
+        {
+            this.InlineMode = "None";               
+            this.IsOptimizable = true;
+            this.IsOptimized = false;
+        }
+
+        /// <summary>
+        /// Inline|Remove|None
+        /// </summary>
+        public string InlineMode { get; set; }
+
+        /// <summary>
+        /// Does this code require processing?
+        /// </summary>
+        public bool IsOptimizable { get; set; }
+
+        /// <summary>
+        /// Has this code been processed?
+        /// </summary>
+        public bool IsOptimized { get; set; }
+    }
+}
